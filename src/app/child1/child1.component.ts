2551df94613842d024f9bfb9018117b2be986537
arqam <48586137+arqam-dev@users.noreply.github.com>
2022-04-22 15:10:10 +0500

    updated

--- src/app/child1/child1.component.ts
+++ src/app/child1/child1.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-// import { prototype } from 'events';
 import { addPrototypeDecorator } from '../decorators/add-prototype.decorator';
-
-// @simpleDecorator
 @Component({
   selector: 'app-child1',
   templateUrl: './child1.component.html',
@@ -12,56 +9,45 @@ import { addPrototypeDecorator } from '../decorators/add-prototype.decorator';
 export class Child1Component implements OnInit {
   constructor() {}
   description = 'I am a boy';
+  objWithHiddenAndReadOnlyProperties = {};
 
   ngOnInit(): void {
     const obj = new Child1Component();
     setTimeout(() => {
       console.log(
         'Prototype Example in Child1 Component: ',
-        (obj as any).stickers // NOTE: EsLint will give err on accessing directly like obj. stickers because although stickers exists but having no type defined.
+        (obj as any).stickers // NOTE: EsLint will give err on accessing directly like obj.stickers because although stickers exists but having no type defined.
       );
-      debugger;
     }, 2000);
+
+    // ------------------------------------- ReadOnly and Hidden Property of an object -------------------------------------
+
+    // Adding readonly property in an object. Can assign the same value again but not a different value to that property.
+    Object.defineProperty(this.objWithHiddenAndReadOnlyProperties, 'name', {
+      value: 'ali',
+      writable: false,
+      enumerable: true,
+    });
+    // Hiding a property of an object
+    Object.defineProperty(this.objWithHiddenAndReadOnlyProperties, 'age', {
+      value: '20',
+      enumerable: false,
+    });
+
+    // (this.objWithHiddenAndReadOnlyProperties as any).name = 'arqam'; // NOTE: This will give error that 'Cannot assign to read only property 'name' of object'.
+    console.log(
+      'objWithHiddenAndReadOnlyProperties name: ',
+      (this.objWithHiddenAndReadOnlyProperties as any).name
+    );
+    console.log(
+      'objWithHiddenAndReadOnlyProperties: ',
+      this.objWithHiddenAndReadOnlyProperties as any
+    );
+
+    for (const abc in this.objWithHiddenAndReadOnlyProperties) {
+      console.log('Enumerable properties of above obj: ', abc);
+    }
   }
 
-  // @confirmable
-  // decoratorFunc(): any {
-  //   alert('Finally, DecoratorFunc called after validation :)');
-  //   return true;
-  // }
+  // ------------------------------------- ************* -------------------------------------
 }
-
-function simpleDecorator(target: any) {
-  console.log('Hello from "simpleDecorator" Decorator');
-
-  // Object.defineProperty(target.prototype, 'value1', {
-  //   value: 100,
-  //   writable: false,
-  // });
-
-  // Object.defineProperty(target.prototype, 'value2', {
-  //   value: 200,
-  //   writable: false,
-  // });
-
-  const description = 'I am a boy';
-  if (description && description.length) {
-    alert('Error from "simpleDecorator" Decorator function');
-    return;
-  }
-}
-
-// function confirmable(
-//   target: Object,
-//   propertyKey: string,
-//   descriptor: PropertyDescriptor
-// ): any {
-//   console.log('Hello from "Confirmable" Decorator');
-
-//   const description = 'I am a boy';
-//   if (description && description.length) {
-//     alert('Error from "Confirmable" Decorator function');
-//     return;
-//   }
-//   return target;
-// }
